8ae193872cc129371b67a2a296f3f852e24db7c5
Lawand-A <76704719+Lawand-A@users.noreply.github.com>
2026-08-13 16:03:42 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    never cached — they must always hit the network. */
 "use strict";
 
-var VERSION = "streamhub-v10";
+var VERSION = "streamhub-v13";
 var SHELL = [
   "./index.html",
   "./manifest.json",
